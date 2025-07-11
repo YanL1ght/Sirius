84c417ee4909ced35f952d35d9f975091155c282
YanLight <yan190408@gmail.com>
2025-07-11 17:37:53 +0300

    base for everything

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
+
+@app.route('/home', methods=["POST", 'GET'])
+@app.route('/', methods=["POST", 'GET'])
+def index():
+    if request.method == "POST":
+        print(request.form.keys())
+        return redirect('/home')
+    elif request.method == 'GET':
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
